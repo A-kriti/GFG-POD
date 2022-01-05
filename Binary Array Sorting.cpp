f825577401eb1263d0c3f50e02b275b4bedaeba9
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-01-05 10:14:49 +0530

    Create Binary Array Sorting.cpp

---
+++ Binary Array Sorting.cpp
@@ -0,0 +1,36 @@
+// ques - https://practice.geeksforgeeks.org/problems/binary-array-sorting-1587115620/1#
+
+void binSort(int A[], int N)
+{
+   //Your code here
+   
+   /**************
+    * No need to print the array
+    * ************/
+    
+    int i=0;
+    int j=N-1;
+    
+    while(i<j){
+        
+        if(A[i]==1 && A[j]==0){
+            
+            int t=A[i];
+            A[i]=A[j];
+            A[j]=t;
+            //swap(A[i],A[j]);
+            i++;
+            j--;
+        }
+        
+        else if(A[i]==0)i++;
+        else if(A[j]==1)j--;
+        
+    }
+    
+    
+    
+    
+    
+}
+    
