1f061109d2c390e66c076aa46efaac1aa25feb22
Akriti <73133055+A-kriti@users.noreply.github.com>
2022-01-20 15:11:07 +0530

    Create Binary number to decimal number.cpp

---
+++ Binary number to decimal number.cpp
@@ -0,0 +1,17 @@
+// ques - https://practice.geeksforgeeks.org/problems/binary-number-to-decimal-number3525/1
+
+class Solution
+{
+	public:
+		int binary_to_decimal(string str)
+		{
+		    // Code here.
+		    int ans=0;
+		    int n=str.size();
+		    for(int i=n-1;i>=0;i--){
+		        if(str[i]=='1')ans+=pow(2,n-1-i);
+		    }
+		    
+		    return ans;
+		}
+};
